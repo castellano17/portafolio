4e5bec188f264647b9416a862f15043109cc3ae2
unknown <castellano17@gmail.com>
2023-03-19 17:27:55 -0600

    about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,63 +1,64 @@
 import React from "react";
+import "./styles/About.css";
 
 const About = () => {
   return (
-    <div id="about" className="shadowBg">
-      About Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
-      aperiam sint at ut eum quae vel laboriosam itaque veniam harum beatae, ad
-      corrupti fuga nemo quia non, perferendis quaerat error hic adipisci
-      laborum? Consequuntur suscipit nulla, labore nesciunt similique cumque.
-      Nam odit fugit tempore ab, quisquam magni deserunt ratione placeat
-      perspiciatis tempora. Tempore, perferendis iure. Maxime, corrupti dolorem!
-      Praesentium magnam aliquam laudantium magni. Nulla eveniet, sapiente
-      reiciendis suscipit eius consectetur. Perferendis odio vitae ab iste
-      exercitationem placeat, molestiae voluptatem voluptas optio sit!
-      Exercitationem corporis doloribus voluptatem! Nesciunt, tenetur debitis
-      animi modi neque a officiis impedit voluptate reprehenderit est quasi
-      illum! Quisquam repellendus optio ad, quam quibusdam quas officiis totam
-      et neque architecto laudantium provident eaque voluptatum quia. Quisquam
-      quis non animi. Laborum temporibus omnis odit atque neque fuga nemo
-      corporis, consequatur repellendus, veritatis vitae error sapiente aut
-      tempore exercitationem quasi accusantium quod! Debitis provident, officia
-      consequatur autem deserunt reiciendis numquam nihil vero rerum temporibus
-      maxime, facere sapiente voluptate minus, saepe beatae. Dolor, facere iure
-      eum accusantium inventore eos, aliquid reiciendis laudantium reprehenderit
-      perferendis doloribus quia veritatis! Dicta quisquam, explicabo cumque
-      fugit modi eveniet, reprehenderit ipsam mollitia tenetur qui nostrum
-      doloremque. Ab debitis quibusdam corrupti aliquam quos placeat aperiam
-      minima, ad natus repudiandae iusto enim aut nesciunt aliquid dicta libero
-      laboriosam rem est fuga suscipit reprehenderit fugiat. Laborum modi quis
-      iure vitae placeat sunt aspernatur assumenda quod ex minus atque corrupti
-      accusamus molestiae, deleniti est dolorem iste. Neque fuga quaerat
-      laboriosam facere accusantium, rem beatae quisquam accusamus itaque nulla
-      mollitia minus excepturi similique alias officia quo aperiam adipisci unde
-      enim eveniet deserunt. Quaerat asperiores, id eaque dolore molestiae
-      similique laudantium ad. Minima magnam asperiores beatae facilis commodi
-      possimus, deserunt architecto! Cupiditate sed, voluptatibus consectetur
-      dignissimos pariatur dolor. Quae ducimus fugit maxime dicta sit, minima
-      mollitia molestias suscipit dolorum nisi similique necessitatibus, eaque
-      eum deserunt harum architecto. Aliquam, amet voluptas perspiciatis
-      deserunt quasi distinctio fugiat. Omnis earum fugit libero obcaecati
-      architecto fuga rerum totam similique iure quia accusamus est aliquid
-      tenetur dolorem aut, officiis et eaque numquam dolores? Quo officiis ab
-      error vero vitae assumenda doloribus vel. In necessitatibus facere saepe
-      totam harum? Temporibus inventore magnam cum. Totam adipisci sint,
-      recusandae perspiciatis, qui tempore doloribus a, libero explicabo
-      excepturi mollitia saepe nemo. Ullam minus doloremque inventore ipsum,
-      ratione praesentium labore alias excepturi perspiciatis, aspernatur et
-      laudantium voluptatum, sunt nihil saepe provident maxime. Blanditiis unde
-      quis debitis corporis nulla recusandae dolor laborum consequatur cum,
-      aliquam laboriosam iusto incidunt, fugit molestias eveniet ullam vitae
-      inventore eaque facere explicabo illo quo aliquid culpa ut? Deleniti esse
-      eum vel. Harum doloremque dolorem voluptate nulla aliquid excepturi,
-      mollitia nisi a quos esse at ab fuga repellat voluptatibus, delectus,
-      sequi optio ipsum architecto quis incidunt! Autem quos veniam assumenda
-      expedita facere ratione consequatur nostrum similique sequi dolores,
-      officiis asperiores, id sed? Incidunt eligendi aspernatur iure molestiae
-      aut a facilis quo natus nemo dicta. Facere aperiam, blanditiis iusto
-      labore debitis quasi ipsam accusamus magnam aliquid ex nihil officiis
-      neque itaque voluptatem modi corporis, quis at expedita? Molestias, enim
-      vitae?
+    <div id="about" className="about shadowBg">
+      <article className="about-me">
+        <h3>
+          Sobre <span>mi</span>
+        </h3>
+        <p>
+          Como un apasionado de la lectura, me atraen los desafíos y disfruto de
+          la competencia sana en todas mis actividades. Creo firmemente que la
+          búsqueda constante de la mejora en múltiples aspectos es clave para el
+          éxito. <br />
+          <br />
+          Tengo una actitud optimista y disfruto motivando a los demás y
+          motivándome a mí mismo. Siempre intento encontrar el lado positivo de
+          cualquier adversidad para evitar frustraciones innecesarias. Me
+          encuentro actualmente en busca de nuevas oportunidades, estas son
+          algunas de las tecnologías que manejo y si gustas contactarme tienes
+          la sección de contacto para hacerlo.
+        </p>
+        <section className="container-skills">
+          <p>
+            <i className="arrow-about bx bxs-right-arrow"></i> HTML5
+          </p>
+          <p>
+            <i className="arrow-about bx bxs-right-arrow"></i> CSS3
+          </p>
+          <p>
+            <i className="arrow-about bx bxs-right-arrow"></i> TailWind CSS
+          </p>
+          <p>
+            <i className="arrow-about bx bxs-right-arrow"></i> JavaScript
+          </p>
+          <p>
+            <i className="arrow-about bx bxs-right-arrow"></i> React
+          </p>
+        </section>
+        <div className="about__button">
+          <a href="./assets/CV-español-canvas.pdf">
+            <img src="./img/descargacv.png" alt="descargar cv" />
+            <p>Download CV</p>
+          </a>
+        </div>
+      </article>
+
+      <article className="studies">
+        <h3>
+          Mis <span>estudios</span>
+        </h3>
+        <div className="studies__container">
+          <div className="studies__list">
+            <img src="./img/platzi.webp" alt="" />
+            <h4>Platzi</h4>
+            <p>2021-2021</p>
+            <p>Curso de Programación Básica</p>
+          </div>
+        </div>
+      </article>
     </div>
   );
 };
