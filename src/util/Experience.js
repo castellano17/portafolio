fb5d7abde81a953c6f234db045b98fdabf20bcac
castellano17 <castellano17@gmail.com>
2024-03-09 15:44:36 -0600

    add experience

--- src/util/Experience.js
+++ src/util/Experience.js
@@ -6,7 +6,8 @@ export const experienceEs = [
       "Participé en un entorno laboral simulado, colaborando con dos equipos multidisciplinarios en América Latina, donde contribuí a la construcción de dos soluciones tecnológicas.",
     tecnologias:
       "Participación en reuniones diarias con el equipo y reuniones semanales con el líder del equipo, brindando una experiencia laboral auténtica.",
-    enlaceGitHub: "https://github.com/castellano17",
+    enlaceGitHub:
+      "https://github.com/No-Country/s12-14-t-java-react/tree/e616654847ff1f4c580ddfc61a4fb061c4c0620c?tab=readme-ov-file",
     enlaceWeb:
       "https://drive.google.com/file/d/1M-MjWT-e8vrrYdGV2xHzde4Y1FPRjtRn/view?usp=sharing",
     img: "img/NoCountry.jpg",
@@ -22,6 +23,28 @@ export const experienceEs = [
     enlaceWeb: "https://certiwise.co/verify/6",
     img: "img/pasantia-Academlo.png",
   },
+  {
+    id: 3,
+    titulo: "Desarrollador web en CodeFy+",
+    descripcion:
+      "Desarrollo aplicaciones web mediante la implementación de tecnologías de vanguardia, manteniendo una comunicación fluida con los clientes para comprender a fondo sus necesidades y expectativas. Colaboración estrechamente con los diseñadores gráficos y otros colegas, asegurando una sinergia efectiva en cada etapa del proceso creativo.",
+    tecnologias:
+      "Next.JS, React.JS, Zudtand, Tailwindcss, HTML, CSS, Tailwind, Figma,GitHub, Jira, Trello, Slack, Notion, .",
+    enlaceGitHub: "https://github.com/castellano17",
+    enlaceWeb: "https://codefyplus.com/",
+    img: "img/CodeFy.png",
+  },
+  {
+    id: 4,
+    titulo: "Desarrollador de app web en ACADEMLO",
+    descripcion:
+      "Durante mi formación en la academia ACADEMLO, adquirí experiencia en el desarrollo de una amplia variedad de aplicaciones web, aprovechando las últimas tecnologías disponibles. Este proceso me permitió fortalecer mis habilidades y conocimientos, abordando desafíos diversos y adquiriendo una perspectiva integral en el diseño y la implementación de soluciones innovadoras.",
+    tecnologias:
+      "Next.JS, React.JS, Node.js, Postgresql, Redux, Tailwindcss, HTML, CSS, Tailwind, Figma,GitHub, Jira, Trello, Slack, Notion, .",
+    enlaceGitHub: "https://github.com/castellano17?tab=repositories",
+    enlaceWeb: "https://certificates.academlo.com/en/verify/40809561884902",
+    img: "img/AcademloCertificado.png",
+  },
 ];
 
 export const experienceEn = [
@@ -32,7 +55,8 @@ export const experienceEn = [
       "I participated in a simulated work environment, collaborating with two multidisciplinary teams in Latin America, where I contributed to the construction of two technological solutions.",
     tecnologias:
       "Daily meetings with the team, and weekly meetings with the Team Leader, real-life experience.",
-    enlaceGitHub: "https://github.com/castellano17",
+    enlaceGitHub:
+      "https://github.com/No-Country/s12-14-t-java-react/tree/e616654847ff1f4c580ddfc61a4fb061c4c0620c?tab=readme-ov-file",
     enlaceWeb:
       "https://drive.google.com/file/d/1M-MjWT-e8vrrYdGV2xHzde4Y1FPRjtRn/view?usp=sharing",
     img: "img/NoCountry.jpg",
@@ -47,5 +71,27 @@ export const experienceEn = [
     enlaceGitHub: "https://github.com/castellano17",
     enlaceWeb: "https://certiwise.co/verify/6",
     img: "img/pasantia-Academlo.png",
+  },
+  {
+    id: 3,
+    titulo: "Web Developer at CodeFy+.",
+    descripcion:
+      "I develop web applications by implementing cutting-edge technologies, maintaining smooth communication with clients to thoroughly understand their needs and expectations. I collaborate closely with graphic designers and other colleagues, ensuring effective synergy at every stage of the creative process.",
+    tecnologias:
+      "Next.JS, React.JS, Zudtand, Tailwindcss, HTML, CSS, Tailwind, Figma,GitHub, Jira, Trello, Slack, Notion, .",
+    enlaceGitHub: "https://github.com/castellano17",
+    enlaceWeb: "https://codefyplus.com/",
+    img: "img/CodeFy.png",
+  },
+  {
+    id: 4,
+    titulo: "Desarrollador de app web en ACADEMLO",
+    descripcion:
+      "During my training at ACADEMLO, I gained experience in developing a wide variety of web applications, utilizing the latest technologies available. This journey allowed me to strengthen my skills and knowledge, tackling diverse challenges and gaining a comprehensive perspective in designing and implementing innovative solutions.",
+    tecnologias:
+      "Next.JS, React.JS, Node.js, Postgresql, Redux, Tailwindcss, HTML, CSS, Tailwind, Figma,GitHub, Jira, Trello, Slack, Notion, .",
+    enlaceGitHub: "https://github.com/castellano17?tab=repositories",
+    enlaceWeb: "https://certificates.academlo.com/en/verify/40809561884902",
+    img: "img/AcademloCertificado.png",
   },
 ];
